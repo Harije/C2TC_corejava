e811d660bd70d9fd765a9148d82e5d0e4b3ade9c
HP <HP@LAPTOP-MA616FDJ>
2023-07-27 22:55:20 +0530

    first commit

---
+++ C2TC_corejava/src/com/cg/dayone/dayone1.java
@@ -0,0 +1,7 @@
+package com.cg.dayone;
+
+public class dayone1 {
+	public static void main(String[]args) {
+		System.out.println("hi hariharan");
+	}
+}
---
+++ C2TC_corejava/src/com/cg/daytwo/daytwo1.java
@@ -0,0 +1,24 @@
+package com.cg.daytwo;
+
+public class daytwo1 {
+	public static void main(String[] args) {
+
+	    int number = 10;
+
+	    // checks if number is less than 0
+	    if (number < 0) {
+	      System.out.println("The number is negative.");
+	    }
+
+	    System.out.println("Statement outside if block");
+	  
+	
+	String language = "Java";
+
+    // if statement
+    if (language == "Java") {
+      System.out.println("Best Programming Language");
+    }
+
+	}
+}
