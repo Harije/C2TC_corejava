0a85d7b6757f0d00c318d295caad6a2b2f09c9ea
HP <HP@LAPTOP-MA616FDJ>
2023-07-27 23:05:18 +0530

    commit

---
+++ C2TC_corejava/src/com/cg/daytwo/methodparameter/methodparameter.java
@@ -0,0 +1,25 @@
+package com.cg.daytwo.methodparameter;
+
+public class methodparameter {
+	// method with no parameter
+	  public void display1() {
+	    System.out.println("Method without parameter");
+	  }
+
+	  // method with single parameter
+	  public void display2(int a) {
+	    System.out.println("Method with a single parameter: " + a);
+	  }
+
+	  public static void main(String[] args) {
+	    
+	    // create an object of Main
+	    methodparameter obj = new methodparameter();
+
+	    // calling method with no parameter
+	    obj.display1();
+	    
+	    // calling method with the single parameter
+	    obj.display2(24);
+	  }
+}
